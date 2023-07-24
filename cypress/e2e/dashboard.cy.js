93f98e09e8ff2401501e5a6df61b8897214c48a9
Ace <archfiendsix@gmail.com>
2023-07-24 08:27:49 +0800

    Initial Dashboard tests

---
+++ cypress/e2e/dashboard.cy.js
@@ -0,0 +1,44 @@
+import DashboardPage from "../pages/DashboardPage";
+
+describe('Login Page Test Suite', () => {
+    beforeEach(() => {
+        cy.login(Cypress.env('email'), Cypress.env('password'));
+        cy.intercept('POST', '/widget/api/submission-count*').as('postCardCount');
+        cy.visit("/")
+        cy.wait('@postCardCount')
+        cy.checkLoading()
+    });
+
+    it('Should apply "All" filter correctly', () => {
+        DashboardPage.applyFilter('ALL')
+    })
+    it('Should apply "DRAFTS" filter correctly', () => {
+        DashboardPage.applyFilter('ALL')
+    })
+    it('Should apply "WITH CUSTOMER" filter correctly', () => {
+        DashboardPage.applyFilter('WITH CUSTOMER')
+    })
+    it('Should apply "READY FOR REVIEW" filter correctly', () => {
+        DashboardPage.applyFilter('READY FOR REVIEW')
+    })
+    it('Should apply "REVIEWED" filter correctly', () => {
+        DashboardPage.applyFilter('REVIEWED')
+    })
+
+    it("Should properly search by Application ID",()=> {
+        DashboardPage.applicationSearch('APPLICATION ID','TRF-QMNJ817IO')
+    })
+    it("Should properly search by Application Name",()=> {
+        DashboardPage.applicationSearch('APPLICANT NAME','ronald laifoo')
+    })
+    it("Should properly search by Application Status",()=> {
+        DashboardPage.applicationSearch('APPLICANT NAME','ronald laifoo')
+    })
+    it.only("Should show card list",()=> {
+        
+        DashboardPage.elements.cardlist.card_count('DRAFTS').should('be.visible').invoke('text').then(text=> {
+            cy.log(text)
+        })
+        
+    })
+});
--- cypress/pages/DashboardPage.js
+++ cypress/pages/DashboardPage.js
@@ -1,13 +1,40 @@
 class DashboardPage {
+    locators =  {
+
+    }
     elements = {
         textWithCustomer: () => cy.get(".gridview .gridview__rows .gridview__row:nth-child(1) .gridview__column:nth-child(7)"),
         textWithoutSet: () => cy.get(".gridview .gridview__rows .gridview__row:nth-child(1) .gridview__column:nth-child(1)"),
-
+        filters: {
+            link: ()=> cy.get('.sidebar li.sidebar-item'),
+            all: ()=> cy.get('.sidebar li.sidebar-item').contains('ALL'),
+            drafts: ()=> cy.get('.sidebar li.sidebar-item').contains('DRAFTS'),
+            with_customer: ()=> cy.get('.sidebar li.sidebar-item').contains('WITH CUSTOMER'),
+            ready_for_review: ()=> cy.get('.sidebar li.sidebar-item').contains('READY FOR REVIEW'),
+            reviewed: ()=> cy.get('.sidebar li.sidebar-item').contains('REVIEWED'),
+        },
+        applicationTable: {
+            input: ()=> cy.get('.gridview .gridview__row-filter'),
+            search_applicationId: ()=> cy.get('.gridview .gridview__row-filter input[placeholder="APPLICATION ID"]')
+        },
+        cardlist: {
+            card_count: (card_label)=> cy.get('.card__list .card__label').contains(card_label).next('.card__count')
+        }
     }
     checkTableAddedApplication = () => {
         cy.checkLoading()
         this.elements.textWithCustomer().should("have.text", "With Customer");
         this.elements.textWithoutSet().should("not.have.text", "Not Set");
+    }
+    applyFilter = (name)=> {
+        cy.intercept('POST', '/widget/api/submission-data*').as('postSubmissionData');
+        this.elements.filters.link().contains(name).click()
+        cy.wait('@postSubmissionData')
+    }
+    applicationSearch=(input, query)=> {
+        cy.intercept('POST', '/widget/api/submission-data*').as('postSubmissionData2');
+        this.elements.applicationTable.input().find(`input[placeholder="${input}"]`).type(query)
+        cy.wait('@postSubmissionData2')
     }
 };
 module.exports = new DashboardPage();
